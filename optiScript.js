3b791a8e7aed5fb2a6e27ef5b5530c5187c1e67f
MirrorInstinct <jarolvaz97@gmail.com>
2024-03-18 21:51:16 -0400

    *

--- optiScript.js
+++ optiScript.js
@@ -3,7 +3,6 @@ const clicked = true;
 function siteButton(){
     if (clicked) {
         console.log("It worked!");
-        alert("It worked!")
         document.getElementById("demo").innerHTML = "Hello World";
     }
 }
